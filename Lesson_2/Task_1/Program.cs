04e1c8a7d7df56b9c6c3181e5609e0987d0af80d
bobrov.da <dmr.bobrov@yandex.ru>
2021-07-25 18:44:41 +0300

    Fix task_1, isOk

--- Lesson_2/Task_1/Program.cs
+++ Lesson_2/Task_1/Program.cs
@@ -6,15 +6,57 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите минимальную темпиратуру:");
-            var min = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите максимальную темпиратуру:");
-            var max = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите минимальную темпиратуру:");
+
+            var readLineMin = Console.ReadLine();
+
+            var min = MinTemp(readLineMin, out var isMin);
+          
+
+            if (!isMin)
+            {
+                Console.WriteLine("Неверный формат, использовать запятую");
+
+                readLineMin = Console.ReadLine();
+                min = MinTemp(readLineMin, out isMin);
+            }
+
+
+            Console.WriteLine("Введите минимальную темпиратуру:");
+
+
+            var readLineMax = Console.ReadLine();
+            var max = MaxTemp(readLineMax, out var isMax);
+
+
+            if (!isMax)
+            {
+                Console.WriteLine("Неверный формат, использовать запятую");
+                readLineMax = Console.ReadLine();
+                max = MaxTemp(readLineMax, out isMax);
+            }
+    
 
             var average = (max + min) / 2;
 
             Console.WriteLine($"Среднесуточная температура: {average}");
+        }
+
+        private static double MaxTemp(string readLine, out bool o)
+        {
+            
+            o = double.TryParse(readLine, out var resultMin);
+
+            return resultMin;
+        }
+
+        private static double MinTemp(string result, out bool isDouble)
+        {
+            isDouble = double.TryParse(result, out var resultMin);
+
+            return resultMin;
         }
     }
 }
