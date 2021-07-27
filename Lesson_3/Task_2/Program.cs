25940865aa25e17d5d593d7d5e9963c6acbb9292
bobrov.da <dmr.bobrov@yandex.ru>
2021-07-27 20:49:11 +0300

    Task_2 isOk

--- Lesson_3/Task_2/Program.cs
+++ Lesson_3/Task_2/Program.cs
@@ -6,7 +6,27 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var directory = new string[5,2]
+            {
+                { "Имя:" ,"Телефон/Email" }, 
+                { "Имя1:", "Телефон/Email1" }, 
+                { "Имя2:", "Телефон/Email2" }, 
+                { "Имя3:", "Телефон/Email3" }, 
+                { "Имя4:", "Телефон/Email4" }
+            };
+
+            for (int i = 0; i < directory.GetLength(0); i++)
+            {
+                for (int j = 0; j < directory.GetLength(1); j++)
+                {
+                    Console.Write(directory[i,j]);
+                }
+
+                Console.WriteLine();
+
+            }
+
+
         }
     }
 }
