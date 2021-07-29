bed882c720b7bb2f31f884d86078e05f04eb85ee
bobrov.da <dmr.bobrov@yandex.ru>
2021-07-29 18:38:08 +0300

    Add task_4, IsNotOk

--- Lesson_3/Task_4/Program.cs
+++ Lesson_3/Task_4/Program.cs
@@ -6,7 +6,29 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var poligon = new int[10,10];
+
+            var little = new int[4, 1];
+            var average = new int[3, 2];
+            var big = new int[1, 4];
+
+            for (var i = 0; i < poligon.GetLength(0); i++)
+            {
+                for (var j = 0; j < poligon.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        poligon[i, j] = j;
+                        Console.Write($"{poligon[i, j]}");
+                    }
+                    else
+                    {
+                        Console.Write($"*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
