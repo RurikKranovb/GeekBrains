d3e70caca806cd8408ad369541012dbed4024e41
bobrov.da <dmr.bobrov@yandex.ru>
2021-08-04 21:36:19 +0300

    Task_1 IsOk

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = {"Фамилия Имя Отчество", "Фамилия Имя Отчество", "Фамилия Имя Отчество", "Фамилия Имя Отчество"};
+
+            foreach (var item in items)
+            {
+                var strings = item.Split(" ");
+
+                GetName(strings[0], strings[1], strings[2]);
+            }
+
+            Console.WriteLine("Введите имя:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            var lastName = Console.ReadLine();
+            Console.WriteLine("Введите отчество:");
+            var patronymic = Console.ReadLine();
+
+            GetName(firstName, lastName, patronymic);
+        }
+
+        private static void GetName(string firstName, string lastName, string patronymic)
+        {
+            Console.WriteLine($"{firstName} {lastName} {patronymic}");
+        }
+    }
+}
